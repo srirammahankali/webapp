0593f82fd071bdecdcb301f67a837922d1111950
slava <dror799@gmail.com>
2025-01-15 17:48:26 +0200

    Multilingual user interface #169.

--- src/common/i18n/i18n.js
+++ src/common/i18n/i18n.js
@@ -1,5 +1,6 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
 
 //import arTranslation from './locales/ar_SA.json';
 //import asTranslation from './locales/as_IN.json';
@@ -35,9 +36,9 @@ import teTranslation from './locales/te_IN.json';
 import zhTranslation from './locales/zh_CN.json';
 
 i18n
+   .use(LanguageDetector)
    .use(initReactI18next)
    .init({
-      lng: 'en',
       fallbackLng: 'en',
       resources: {
          bn: {
@@ -71,10 +72,6 @@ i18n
             translation: zhTranslation,
          },
       },
-      interpolation: {
-         escapeValue: false,
-      },
    });
 
 export default i18n;
-
