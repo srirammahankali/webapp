da3ed895f976cad3aeaf9bb7a18085b3211856df
srirammahankali7 <32706903+srirammahankali@users.noreply.github.com>
2025-01-24 05:14:48 +0530

    Update Contact.jsx

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -16,8 +16,8 @@ const Contact = () => {
 			<form
 				className='w-2/5 flex flex-col gap-5'
 				// onSubmit={fromSubmitHandler}
-				action='https://formsubmit.co/info@saayamforall.com'
-				method='POST'
+				action="https://formsubmit.co/info@saayamforall.org" 
+				method="POST"
 			>
 				<label
 					for='firstName'
